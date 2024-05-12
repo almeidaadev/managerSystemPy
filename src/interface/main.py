e8f9ec65b8b096fe2bb0a59a3276bc14ba0afe42
almeidaadev <unkn0wn01127800@gmail.com>
2024-05-12 04:35:17 -0300

    update path

--- src/interface/main.py
+++
@@ -1,9 +0,0 @@
-from customtkinter import *
-
-app = CTk()
-app.geometry("800x600")
-
-btn = CTkButton(master=app, text='Options', corner_radius=32)
-btn.place(relx=0.5, rely=0.5, anchor=CENTER)
-
-app.mainloop()
