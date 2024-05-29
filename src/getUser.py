9f6ece3a26966257dd1f3ef2920efebfbbb7b7e1
almeidaadev <unkn0wn01127800@gmail.com>
2024-05-29 12:50:17 -0300

    removeComment

--- src/getUser.py
+++ src/getUser.py
@@ -7,15 +7,4 @@ def ver_ipv4():
         return ip_address
     except socket.error as e:
         print(f"Erro ao obter endereço IPv4: {e}")
-        return None
-
-"""
-ipv4 = ver_ipv4()
-if ipv4:
-    print(f"Endereço IPv4: {ipv4}")
-else:
-    print("Não foi possível obter o endereço IPv4.")
-
-username = user_computer()
-print(f"Nome de usuário: {username}")
-"""
+        return
